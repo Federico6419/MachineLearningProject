9accb4f5ba5f3878c7c6674b8213ddd5ccc0c80f
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2023-10-12 19:36:54 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,4 +6,4 @@ EPSILON_DECAY = 0.99
 LR = 0.0005      #Learning Rate of the Neural Network
 BUFFER_SIZE = 100000
 
-DEVICE ="cuda" if torch.cuda.is_available() else "cpu"
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
