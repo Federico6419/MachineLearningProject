c82973775aee47b7e9197d3ea767b388ca7fddc1
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2023-10-18 10:11:45 +0200

    Update config.py

--- config.py
+++ config.py
@@ -13,3 +13,18 @@ TARGET_FREQ_UPDATE = 10
 USE_QTABLE = False
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+
+#Manage Checkpoints
+def save_model(model, optimizer, episode):  
+    torch.save({
+        "state_dict": model.state_dict(),
+        "optimizer": optimizer.state_dict(),
+    }, CHECKPOINT)
+
+def load_model(file, model, optimizer):
+    model_check = torch.load(file, map_location=DEVICE)
+    model.load_state_dict(model_check["state_dict"])
+    optimizer.load_state_dict(model_check["optimizer"])
+    
+    for param_group in optimizer.param_groups:
+        param_group["lr"] = LR
