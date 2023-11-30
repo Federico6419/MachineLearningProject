31537eb424ed9265ac19dcefcf4d6f850f460429
Federico6419 <102321945+Federico6419@users.noreply.github.com>
2023-11-30 12:53:20 +0100

    Update config_test.py

--- config_test.py
+++ config_test.py
@@ -1,19 +1,21 @@
 import torch
 
 NUM_EPISODES = 100      #Number of episodes
-LR = 0.0005      #Learning Rate of the Neural Network
+LR = 0.0005             #Learning Rate of the Neural Network
 
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"        #Choose the Device
 
-#Manage Checkpoints
-CHECKPOINT_FOLDER = "Checkpoints/checkpoint.pth.tar"
+#Checkpoints managenent
+CHECKPOINT_FOLDER = "Checkpoints/checkpoint.pth.tar"            #This is the file that contains the weights to use in testing
 
+#Function that saves current weights
 def save_model(model, optimizer, episode):  
     torch.save({
         "state_dict": model.state_dict(),
         "optimizer": optimizer.state_dict(),
     }, CHECKPOINT_FOLDER)
 
+#Function that loades pre-trained weights
 def load_model(file, model, optimizer):
     model_check = torch.load(file, map_location=DEVICE)
     model.load_state_dict(model_check["state_dict"])
